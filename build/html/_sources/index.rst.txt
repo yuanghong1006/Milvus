8f6272736f7d7b3d9c1ba84c71cada190cd24ccd
unknown <yuanghong1006@163.com>
2025-05-09 15:47:16 +0800

    first commit

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,23 @@
+.. sphinx project documentation master file, created by
+   sphinx-quickstart on Fri May  9 14:38:02 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to sphinx project's documentation!
+==========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   
+   markdown_demo.md
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ source/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"
+#html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+project = 'sphinx project'
+copyright = '2025, yuanghong'
+author = 'yuanghong'
+release = 'yuanghong0509'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+#html_theme = 'alabaster'
+html_static_path = ['_static']
+extensions = ['recommonmark','sphinx_markdown_tables'] 
